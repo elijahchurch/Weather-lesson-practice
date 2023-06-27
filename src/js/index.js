9331fd7a927ae19ac9a0b8b4fb62989ef4a3de50
Jason E. Church <elijahchurch@gmail.com>
2023-06-27 10:33:45 -0700

    refactor index.js to include promise

--- src/js/index.js
+++ src/js/index.js
@@ -3,36 +3,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import './css/styles.css';
 
 // Business Logic
-
 function getWeather(city) {
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
-
-    request.addEventListener("loadend", function () {
-        const response = JSON.parse(this.responseText);
-        if (this.status === 200) {
-            printElements(response, city);
-        } else {
-            printError(this, response, city);
-        }
+    let promise = new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest();
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+        request.addEventListener("loadend", function () {
+            const response = JSON.parse(this.responseText);
+            if (this.status === 200) {
+                resolve([response, city]);
+            } else {
+                reject([this, response, city]);
+            }
+        });
+        request.open("GET", url, true);
+        request.send();
     });
 
-    request.open("GET", url, true);
-    request.send();
-    request.addEventListener("readystatechange", function () {
-        console.log(this.readyState);
+    promise.then(function (response) {
+        printElements(response);
+    }, function (errorMessage) {
+        printError(errorMessage);
     });
 }
-
 // UI Logic
 
-function printError(request, apiResponse, city) {
-    document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}:  ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function printError(error) {
+    document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${error[2]}:  ${error[0].status} ${error[0].statusText}: ${error[1].message}.`;
 }
 
-function printElements(apiResponse, city) {
-    document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-    The temperature in Farenheight is ${apiResponse.main.temp} degrees. A brief description for today's weather in ${city} is ${apiResponse.weather[0].description}. Sunrise today is: ${new Date(apiResponse.sys.sunrise * 1000)}`;
+function printElements(data) {
+    document.querySelector('#showResponse').innerText = `The humidity in ${data[1]} is ${data[0].main.humidity}%.
+    The temperature is ${data[0].main.temp} degrees. A brief description for today's weather in ${data[1]} is ${data[0].weather[0].description}. Sunrise today is: ${new Date(data[0].sys.sunrise * 1000)}.`;
 }
 
 function handleFormSubmission(event) {
